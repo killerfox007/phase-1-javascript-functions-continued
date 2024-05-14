27416b1bec1548c45102e13485aa53ad25467ea3
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-14 10:29:05 -0400

    done

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ return (`This Saturday, I want to ${saturday}!`);
 function mondayWork(work = 'go to the office'){
     return (`This Monday, I will ${work}.`)
 }
-function wrapAdjective(){
-    return function(name = 'special', thing = "*") {
+function wrapAdjective(thing = "*"){
+    return function(name = 'special') {
         //const thing ="*"
         return `You are ${thing}${name}${thing}!`
     }
