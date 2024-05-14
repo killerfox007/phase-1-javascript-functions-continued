3b64b718629558ae5447e5305a3e8b5706321c44
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-13 22:31:11 -0400

    how to make it change inside the function?

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(saturday = 'roller-skate'){
+return (`This Saturday, I want to ${saturday}!`);
+}
+
+function mondayWork(work = 'go to the office'){
+    return (`This Monday, I will ${work}.`)
+}
+function wrapAdjective(){
+    return function(name = 'special', thing = "*") {
+        //const thing ="*"
+        return `You are ${thing}${name}${thing}!`
+    }
+}
